a170ed252baa39d7a84cac48b2b33ec0356e96ac
maksym <maksimka37@gmail.com>
2022-03-15 19:56:56 +0200

    initial commit

--- src/main/java/com/example/crypto/controller/CryptoCurrencyController.java
+++ src/main/java/com/example/crypto/controller/CryptoCurrencyController.java
@@ -3,10 +3,9 @@ package com.example.crypto.controller;
 import com.example.crypto.entity.CryptoVault;
 import com.example.crypto.enums.CryptoVaultEnum;
 import com.example.crypto.repository.CryptoVaultRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -14,17 +13,24 @@ import javax.annotation.Resource;
 @RestController
 public class CryptoCurrencyController {
 
-    @Autowired
     CryptoVaultRepository repository;
 
-    @GetMapping("/cryptocurrencies/minprice{name}")
-    public ResponseEntity<CryptoVault> getVaultMinPrice(@PathVariable(value = "name") String currencyName) {
-        CryptoVault cryptoVault = repository.findTopByVaultOrderByValueDesc(CryptoVaultEnum.valueOf(currencyName));
-        return ResponseEntity.ok(cryptoVault);
+    @GetMapping("/cryptocurrencies/minprice")
+    public ResponseEntity<String> getVaultMinPrice(@RequestParam(value = "name") CryptoVaultEnum currencyName) {
+        //todo fix connection to db
+        CryptoVault cryptoVault = repository.findTopByVaultOrderByValueDesc(currencyName);
+        return ResponseEntity.ok("cryptoVault");
     }
 
-//    @Resource(name = "cryptoVaultRepository")
-//    public void setRepository(CryptoVaultRepository repository) {
-//        this.repository = repository;
-//    }
+    @GetMapping("/cryptocurrencies/maxprice")
+    public ResponseEntity<String> getVaultMaxPrice(@RequestParam(value = "name") CryptoVaultEnum currencyName) {
+        //todo fix connection to db
+        CryptoVault cryptoVault = repository.findTopByVaultOrderByValueAsc(currencyName);
+        return ResponseEntity.ok("cryptoVault");
+    }
+
+    @Resource(name = "cryptoVaultRepository")
+    public void setRepository(CryptoVaultRepository repository) {
+        this.repository = repository;
+    }
 }
